a305d697b3b82b687459c117f972d1f258036c10
NoLogicTech <80648342+NoLogicTech@users.noreply.github.com>
2021-06-30 16:59:31 +0100

    polindo

--- NovoGestor/NovoGestor/Startup.cs
+++ NovoGestor/NovoGestor/Startup.cs
@@ -35,7 +35,7 @@ namespace NovoGestor
             services.AddDbContext<CNGestaoProjetosContext>(options => options.UseSqlServer(connection));
             services.AddHttpClient<ApiService>(client =>
            {
-               client.BaseAddress = new Uri("http://localhost:50421/");
+               client.BaseAddress = new Uri("http://localhost:19251");
                client.DefaultRequestHeaders.Add("Accept", "application/+json");
            });
             
