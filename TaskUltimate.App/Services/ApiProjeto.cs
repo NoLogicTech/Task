8f5ad5a69708a0c47e4979cf00353cbb970c1970
NoLogicTech <80648342+NoLogicTech@users.noreply.github.com>
2021-07-05 14:57:58 +0100

    grande update
    nesse update muitas coisa foram reconstruida.
    
    Foi reconstruído os formulários agora é necessário incluir menos dados, pois vários já são feitos automaticamente.
    
    Novas paginas foram contruidas: Pojetos(aonde fica a lista de todos os projetos) e PageTarefa( dependendo do valor passado pela url é mostrado uma lista de tarefas diferente).
    
    Os componentes do drag and drop agora são diferentes e masi compactos e menos complexos.
    
    Foi adicionado a Api de Projetos.
    
    Foram aplicados novas funcionalidades na Api de Tarefas aonde posso pedir agora apenas por Tarefas de uma determinado projeto.
    
    Foi feito um pequeno trabalho de css para embelezar um pouco a pagina.
    
    Foi refeito os modals e foi feito a integração da biblioteca radzen.
    
    também foi trabalhado um pouco a validação do formulário que se for introduzido um valor incorreto pode retornar alguma mensagem.

---
+++ TaskUltimate.App/Services/ApiProjeto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskUltimate.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace TaskUltimate.App.Services
+{
+    public class ApiProjeto
+    {
+        private HttpClient _httpClient;
+        public ApiProjeto(HttpClient client)
+        {
+            _httpClient = client;
+        }
+
+        public async Task<List<Projeto>> GetAsync()
+        {
+            var result = await _httpClient.GetAsync("api/projeto");
+            using var responseStream = await result.Content.ReadAsStreamAsync();
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return await JsonSerializer.DeserializeAsync<List<Projeto>>(responseStream, options);
+        }
+
+        public async Task<Projeto> GetForIdAsync(int id)
+        {
+            var result = await _httpClient.GetAsync($"api/projeto/{id}");
+            using var responseStream = await result.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return await JsonSerializer.DeserializeAsync<Projeto>(responseStream, options);
+        }
+        public async Task PutAsync(int id, Projeto value)
+        {
+            await _httpClient.PutAsJsonAsync<Projeto>($"api/projeto/{id}", value);
+        }
+
+        public async Task Post(Projeto value)
+        {
+            await _httpClient.PostAsJsonAsync<Projeto>("api/tarefas/", value);
+        }
+
+        public async Task Delete(int id)
+        {
+            await _httpClient.DeleteAsync($"api/projeto/{id}");
+        }
+    }
+}
---
+++ TaskUltimate.App/Services/ApiTarefa.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskUltimate.Models;
+using System.Net.Http.Json; 
+using System.Net.Http;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TaskUltimate.App.Services
+{
+
+    public class ApiTarefa
+    {
+        public HttpClient _httpClient;
+        public ApiTarefa(HttpClient client)
+        {
+            _httpClient = client;
+        }
+        public async Task<List<Tarefa>> GetAsync()
+        {
+            var result = await _httpClient.GetAsync("api/tarefas/");
+            using var responseStream = await result.Content.ReadAsStreamAsync();
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return await JsonSerializer.DeserializeAsync<List<Tarefa>>(responseStream, options);
+        }
+        public async Task<Tarefa> GetForIdAsync(int id)
+        {
+            var result = await _httpClient.GetAsync($"api/tarefas/{id}");
+            using var responseStream = await result.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return await JsonSerializer.DeserializeAsync<Tarefa>(responseStream, options);
+        }
+
+        public async Task<List<Tarefa>> GetForProjectIdAsync(int id)
+        {
+            var result = await _httpClient.GetAsync($"api/tarefas/projeto/{id}");
+            using var responseStream = await result.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return await JsonSerializer.DeserializeAsync<List<Tarefa>>(responseStream, options);
+        }
+
+        public async Task PutAsync(int id, Tarefa trf)
+        {
+            await _httpClient.PutAsJsonAsync<Tarefa>($"api/tarefas/{id}", trf);
+        }
+
+        public async Task Post(Tarefa trf)
+        {
+            await _httpClient.PostAsJsonAsync<Tarefa>("api/tarefas/", trf);
+        }
+
+        public async Task Delete(int id)
+        {
+            await _httpClient.DeleteAsync($"api/tarefas/{id}");
+        }
+    }
+}
